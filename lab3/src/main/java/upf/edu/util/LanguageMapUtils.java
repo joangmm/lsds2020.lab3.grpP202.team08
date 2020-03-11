67c48cbd22c08fddc20e5c5815633187ad5105de
Rjr <ramon.valles02@estudiant.upf.edu>
2020-03-11 19:23:04 +0100

    buildLanguageMap Done + Dependencies version fixed

--- lab3/src/main/java/upf/edu/util/LanguageMapUtils.java
+++ lab3/src/main/java/upf/edu/util/LanguageMapUtils.java
@@ -1,11 +1,17 @@
 package upf.edu.util;
-
+import java.util.*;
+import scala.Tuple2;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 
 public class LanguageMapUtils {
 
     public static JavaPairRDD<String, String> buildLanguageMap(JavaRDD<String> lines) {
-        return null;// IMPLEMENT ME
+        String s = "\t";
+        
+        JavaPairRDD<String, String> languageMap = lines.mapToPair(l -> new Tuple2<>((l.split(s))[1],(l.split(s))[2]));
+        
+        return languageMap;
+        //return null;// IMPLEMENT ME
     }
 }
