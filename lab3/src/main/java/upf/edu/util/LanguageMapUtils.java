9512c9c6eabf2eb8370e53b80eeae824b4fb9a42
Rjr <ramon.valles02@estudiant.upf.edu>
2020-03-13 18:13:25 +0100

    buildLanguageMap improved

--- lab3/src/main/java/upf/edu/util/LanguageMapUtils.java
+++ lab3/src/main/java/upf/edu/util/LanguageMapUtils.java
@@ -8,8 +8,11 @@ public class LanguageMapUtils {
 
     public static JavaPairRDD<String, String> buildLanguageMap(JavaRDD<String> lines) {
         String s = "\t";
+        String empty = "";
         
-        JavaPairRDD<String, String> languageMap = lines.mapToPair(l -> new Tuple2<>((l.split(s))[1],(l.split(s))[2]));
+        JavaPairRDD<String, String> languageMap = lines
+                .filter(e -> !e.split(s)[1].equals(empty))
+                .mapToPair(l -> new Tuple2<>((l.split(s))[1],(l.split(s))[2]));
         
         return languageMap;
         //return null;// IMPLEMENT ME
