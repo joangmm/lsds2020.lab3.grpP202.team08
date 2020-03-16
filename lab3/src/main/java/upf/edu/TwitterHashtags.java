95b8afb56e69c450818867eb8223a70e0b23d2bf
Rjr <ramon.valles02@estudiant.upf.edu>
2020-03-16 20:36:46 +0100

    Table put items in progress

--- lab3/src/main/java/upf/edu/TwitterHashtags.java
+++ lab3/src/main/java/upf/edu/TwitterHashtags.java
@@ -5,18 +5,37 @@ import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.twitter.TwitterUtils;
+import org.apache.spark.streaming.api.java.JavaDStream;
 import twitter4j.Status;
 import twitter4j.auth.OAuthAuthorization;
 import upf.edu.util.ConfigUtils;
-
 import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.client.builder.AwsClientBuilder;
 
 public class TwitterHashtags {
 
+    final static String endpoint = "dynamodb.us-east-1.amazonaws.com";
+    final static String region = "us-east-1";
+    final static String tableName = "test_table";
+    
     public static void main(String[] args) throws InterruptedException, IOException {
         String propertiesFile = args[0];
         OAuthAuthorization auth = ConfigUtils.getAuthorizationFromFileProperties(propertiesFile);
 
+                
         SparkConf conf = new SparkConf().setAppName("Real-time Twitter Example");
         JavaStreamingContext jsc = new JavaStreamingContext(conf, Durations.seconds(5));
         // This is needed by spark to write down temporary data
@@ -25,7 +44,48 @@ public class TwitterHashtags {
         final JavaReceiverInputDStream<Status> stream = TwitterUtils.createStream(jsc, auth);
 
         // <IMPLEMENT ME>
+        JavaDStream<String> textFromTweets = stream.map(F-> F.getText());
+        JavaDStream<String> NormalisedtextFromTweets = textFromTweets.map(G-> G.trim().toLowerCase());
+        
+        
+        ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
+        try {
+            credentialsProvider.getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException(
+                    "Cannot load the credentials from the credential profiles file. " +
+                    "Please make sure that your credentials file is at the correct " +
+                    "location (/home/rjr/.aws/credentials), and is in valid format.",
+                    e);
+        }
+        final AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint, region)   )
+                .withCredentials(credentialsProvider)
+                .build();
+        final DynamoDB dynamoDB = new DynamoDB(client);
+        final Table dynamoDBTable = dynamoDB.getTable(tableName);
+        
+        
+        // Create item
+        int year = 2015;
+        String title = "The Big New Movie";
+        final Map<String, Object> infoMap = new HashMap<String, Object>();
+        infoMap.put("plot", "Nothing happens at all.");
+        infoMap.put("rating", 0);
+        try {
+            System.out.println("Adding a new item...");
+            PutItemOutcome outcome = dynamoDBTable
+                .putItem(new Item().withPrimaryKey("year", year, "title", title).withMap("info", infoMap));
 
+            System.out.println("PutItem succeeded:\n" + outcome.getPutItemResult());
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to add item: " + year + " " + title);
+            System.err.println(e.getMessage());
+        }
+        
+        NormalisedtextFromTweets.print();
         // Start the application and wait for termination signal
         jsc.start();
         jsc.awaitTermination();
